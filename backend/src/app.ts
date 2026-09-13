af8fc8b30d7136f51274068ce9895e80d839e2d4
uksinha-sudo <sinhanikhil377@gmail.com>
2026-09-13 20:00:49 +0530

    feat: add Dockerfile for application containerization and serve frontend in production

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import path from 'path'
+
 import { clerkMiddleware } from '@clerk/express'
 
 import authRoutes from './routes/authRoutes'
@@ -27,5 +29,15 @@ app.use("/api/users", userRoutes);
 
 //error handlers must come after all the routes and other middlewares so they can catch errors passed with next(err) or thrown inside async handlers.
 app.use(errorHandler);
+
+
+//server front-end in production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../../web/dist")))
+
+    app.get("/{*any}", (_, res) => {
+        res.sendFile(path.join(__dirname, "../../web/dist/index.html"));
+    });
+}
 
 export default app
